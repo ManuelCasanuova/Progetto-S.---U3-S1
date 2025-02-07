6f945eac316f31f605869a02a78ec43061bfd43a
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-02-07 16:20:57 +0100

    Inserirte note

--- src/Components/SingleFilm.jsx
+++ src/Components/SingleFilm.jsx
@@ -7,6 +7,10 @@ const SingleFilm = (props) => {
         src={props.film.Poster}
         alt={props.film.Title}
         style={{ width: "100%", height: "400px", objectFit: "cover" }}
+
+        //Ho fissato le immagini a dimensioni tali da vedere le locandine
+        //per la loro interezza. Non mi piaceva assolutamente vederle tagliate.
+        //Per rispettare il mockup avrei potuto riddure la Height
       />
     </Col>
   );
